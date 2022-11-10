45ee71f2b92bae1240d6cbf995ffd2b4995da531
dadodasyra <dadodasyraminecraftien@gmail.com>
2022-11-11 00:21:49 +0100

    Fix upload for new machine (not tested)

--- commands/hidden/upload.js
+++ commands/hidden/upload.js
@@ -1,24 +1,24 @@
 const config = require("../../config.json");
 const {PermissionsBitField} = require("discord.js");
-const fs = require('fs');
 const shell = require('shelljs')
 
 module.exports.run = async(client, message, args) => {
-    console.log("hello world")
     if (!config.owners.includes(message.author.id)) return message.channel.send(`**Seulement le bg peut faire ça** :sunglasses:`)
 
+    let plugin = args[1] ?? "MiniCoreV6";
+    let path = args[2] ?? "plugins";
+
+    let SSHcommand = "";
     if(args[0]) {
-        try {
-            await fs.cpSync("/root/v6/miscs/" + args[0]+"/plugins/MiniCoreV6", "/root/v6/upload/MiniCoreV6", { recursive: true })
-            message.reply('success!')
-        } catch (err) {
-            message.reply(err)
-        }
+        SSHcommand = "cp /root/servers/miscs/" + args[0] + "/plugins/"+plugin+"/ /root/servers/upload/ -r && ";
     }
 
-    shell.cd("/root/v6/upload/");
-    shell.exec('./upload.sh plugins MiniCoreV6');
-    message.reply("On va dire que ça a marché hein");
+    SSHcommand += "cd /root/servers/upload/ && ./upload.sh " + path + " " + plugin;
+    let replied = shell.exec("ssh root@v6.histeria.fr -p 25 \"" + SSHcommand + "\"", {silent:true}).stdout;
+    message.reply(replied);
+    //count lines of replied
+    let lines = replied.split("\r").length;
+    message.reply("On va dire que ça a marché hein, réponse de "+lines+" lignes");
 };
 
 module.exports.config = {
