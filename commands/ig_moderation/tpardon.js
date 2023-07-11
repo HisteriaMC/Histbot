148b870e8d31d71484e51fc593c571d5505b2b1b
dadodasyra <dadodasyraminecraftien@gmail.com>
2023-07-11 23:37:33 +0200

    Update tpardon so that it ask the DB itself

--- commands/ig_moderation/tpardon.js
+++ commands/ig_moderation/tpardon.js
@@ -1,21 +1,41 @@
 const hidden = require("../../hidden.json");
 const {PermissionFlagsBits} = require("discord-api-types/v10");
 
-module.exports.run = async(client, message, args) => {
-    if (!hidden.rcon.servers.includes(message.channel.guild.id)) return message.channel.send("Petit malin va ! Tu croyais me berner comme ça");
+module.exports.run = (client, message, args) => {
+    if (!hidden.rcon.servers.includes(message.channel.guild.id))
+        return message.channel.send("Petit malin va ! Tu croyais me berner comme ça");
 
-    let rcon = client.commands.get("rcon");
-    rcon.config.rconfunc(19503, "tpardon " + args.join(' '), "fac1", message);
+    const pseudo = args[0];
+
+    client.mysqlingame.query('SELECT * FROM ban WHERE player = ?', [pseudo], (err, results) => {
+        if (err) {
+            console.error(err);
+            return message.channel.send('Une erreur s\'est produite lors de la recherche du joueur banni.');
+        }
+
+        if (!results || results.length === 0) {
+            return message.channel.send('Ce joueur n\'est pas banni.');
+        }
+
+        client.mysqlingame.query('DELETE FROM ban WHERE player = ?', [pseudo], (err) => {
+            if (err) {
+                console.error(err);
+                return message.channel.send('Une erreur s\'est produite lors du débannissement du joueur.');
+            }
+
+            message.channel.send(`Le joueur ${pseudo} a été débanni avec succès !`);
+        });
+    });
 };
 
 module.exports.config = {
-    name: "tpardon",
-    description: "Deban un joueur en jeu",
-    format: "tpardon <pseudo>",
+    name: 'tpardon',
+    description: 'Deban un joueur en jeu',
+    format: 'tpardon <pseudo>',
     canBeUseByBot: false,
-    category: "Moderation",
-    alias: ["pardon", "tunban"],
+    category: 'Moderation',
+    alias: ['pardon', 'tunban'],
     permission: PermissionFlagsBits.BanMembers,
     needed_args: 1,
-    args: {pseudo: "string"}
+    args: { pseudo: 'string' }
 };
